40c1d5b7342389c43a4d4ce85a784b0376cbf36f
UlyanovAA <ulyanovalexsandr@mail.ru>
2025-06-01 17:57:01 +0300

    Add new feature

--- UlyanovAA/Program.cs
+++ UlyanovAA/Program.cs
@@ -8,11 +8,37 @@ namespace UlyanovAA
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static public double Sqr(double X)
         {
+            return X * X;
+        }
+
+        static void TestSqr()
+        {
+            (double input, double expected)[] tests = new[]
+            {
+            (2.0, 4.0),
+            (-3.0, 9.0),
+            (0.0, 0.0),
+            (1.5, 2.25),
+            (0.1, 0.01)
+            };
+
+            foreach (var test in tests)
+            {
+                double result = Sqr(test.input);
+                bool passed = Math.Abs(result - test.expected) < 1e-10; // Учитываем погрешность для дробных чисел
+                Console.WriteLine($"Вход: {test.input}, Ожидаем: {test.expected}, Вывод: {result}, {(passed ? "+" : "-")}");
+            }
+        }
+
+        static void Main()
+        {
+            TestSqr();
         }
     }
 }
 //UlyanovAA Am-1-24
 
 //solve task2
+
