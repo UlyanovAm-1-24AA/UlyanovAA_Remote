6cb3c7841c53edb93904ddef2e4e8878261138f6
UlyanovAA <ulyanovalexsandr@mail.ru>
2025-06-05 16:25:20 +0300

    (Changes for Programm.cs conflict_UlyanovAA)

--- UlyanovAA/Program.cs
+++ UlyanovAA/Program.cs
@@ -4,15 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UlyanovAA
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+
 //UlyanovAA Am-1-24
+
+
+//solve task1
 
 //solve task2
